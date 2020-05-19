4e07fb535b6affa5c703ad781e9e2943172021cd
kallistahalim <kallistahalim@gmail.com>
2020-05-19 11:35:25 -0700

    inquirer is added

--- matlibs.js
+++ matlibs.js
@@ -1,9 +1,28 @@
+var inquirer = require("inquirer");
+
 function MatLib(noun, verb, adjective) {
     this.noun = noun,
-    this.verb = verb,
-    this.adjective = adjective,
-    this.Print = function () {
-        console.log("I have 10 [noun]. I will [verb] them [adjective]");
-    }
+        this.verb = verb,
+        this.adjective = adjective,
+        this.Print = function () {
+            console.log("I have 10 [noun]. I will [verb] them [adjective]");
+        }
 }
 
+var questions = [{
+        name: "noun",
+        message: "Input a noun: "
+    },
+    {
+        name: "verb",
+        message: "Input a verb: "
+    },
+    {
+        name: "adjective",
+        message: "Input an adjective: "
+    }
+]
+
+inquirer.prompt(questions).then(function (answers) {
+    console.log(answers)
+});
