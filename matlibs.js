7bd81a16c82a361e23f6a143b8e7bbc6b697234a
kallistahalim <kallistahalim@gmail.com>
2020-05-18 18:53:53 -0700

    base done

---
+++ matlibs.js
@@ -0,0 +1,9 @@
+function MatLib(noun, verb, adjective) {
+    this.noun = noun,
+    this.verb = verb,
+    this.adjective = adjective,
+    this.Print = function () {
+        console.log("I have 10 [noun]. I will [verb] them [adjective]");
+    }
+}
+
